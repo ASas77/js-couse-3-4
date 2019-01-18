ef4453b48fe31efc223d4a6b39950470573dad4b
ASas77 <41167252+ASas77@users.noreply.github.com>
2019-01-19 00:53:58 +0100

    Add files via upload

---
+++ 01-intro-js/02-count-buttons/scripts/app.js
@@ -0,0 +1,34 @@
+(function(){
+
+    var labelFirst = document.querySelector('#label-first');
+    var firstButton = document.querySelector('#btn-first');
+    var countFirst =0;
+
+    var labelSecond = document.querySelector('#label-second');
+    var secondButton = document.querySelector('#btn-second');
+    var countSecond =0;
+
+    var firstButtonMinus = document.querySelector('#btn-firstMinus');
+
+    var secondButtonMinus = document.querySelector('#btn-secondMinus');
+
+    firstButton.addEventListener('click', function(){
+        countFirst++;
+        alert('first  '+ countFirst)
+    })
+
+    secondButton.addEventListener('click', function(){
+        countSecond++;
+        alert('second  '+ countSecond)
+    })
+    firstButtonMinus.addEventListener('click', function(){
+        countFirst--;
+        alert('first  '+ countFirst)
+    })
+    secondButtonMinus.addEventListener('click', function(){
+        countSecond--;
+        alert('first  '+ countSecond)
+    })
+
+    ;
+})()
