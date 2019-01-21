c952e8ffa3b4ff15606dd188c9f7903611d76c5b
ASas77 <alezi77@rambler.ru>
2019-01-21 21:14:50 +0100

    wip

--- 01-intro-js/bank-account.js
+++ 01-intro-js/bank-account.js
@@ -2,12 +2,17 @@
 function getNewBankAccount() {
     return {
         balance: 0,
+        creditLimit : 0,
         deposit: function(amount){
-            // deposit money here
+            this.balance = this.balance+amount;
         },
     
-        widthraw: function(money){
-            // widthraw money here
+        withdraw: function(money){
+            this.balance = this.balance - money;
+            // withdraw money here
+        },
+        setCreditLimit : function ( creditLimit ) {
+            this.creditLimit = creditLimit;
         }
     }
 } 
--- 02-count-buttons/scripts/app.js
+++ 02-count-buttons/scripts/app.js
@@ -5,9 +5,28 @@
 
     var labelSecond = document.querySelector('#label-second');
     var secondButton = document.querySelector('#btn-second');
+    var countSecond =0;
 
-    firstButton.addEventListener('click', function(e){
-        alert('first')
+    var firstButtonMinus = document.querySelector('#btn-firstMinus');
+
+    var secondButtonMinus = document.querySelector('#btn-secondMinus');
+
+    firstButton.addEventListener('click', function(){
+        countFirst++;
+        alert('first  '+ countFirst)
+    })
+
+    secondButton.addEventListener('click', function(){
+        countSecond++;
+        alert('second  '+ countSecond)
+    })
+    firstButtonMinus.addEventListener('click', function(){
+        countFirst--;
+        alert('first  '+ countFirst)
+    })
+    secondButtonMinus.addEventListener('click', function(){
+        countSecond--;
+        alert('first  '+ countSecond)
     })
 
     secondButton.addEventListener('click', function(e){
